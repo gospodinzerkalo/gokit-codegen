6c0c7daf49da6cf6a5c715487cdeecf31ec77faa
gospodinzerkalo <darhan020818@gmail.com>
2022-03-07 23:01:41 +0600

    tags for select

--- domain/user.go
+++ domain/user.go
@@ -1,7 +1,7 @@
 package domain
 
 type User struct {
-	ID   int	`json:"id"`
-	Name string	`json:"name" col:"name"`
+	ID   int	`json:"id" sel:"id"`
+	Name string	`json:"name" col:"name" sel:"name"`
 	Users string `json:"-" table_name:"users"'`
 }
